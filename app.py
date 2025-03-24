057885c8464e0795c3d6e468c96595648a5222f4
Tran Phuoc Sang <transhack09@gmail.com>
2025-03-24 10:04:10 +0700

    style: Update README and .env

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# from operator import le
 import streamlit as st
 import torch
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -8,38 +7,34 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import os
-# import logging
-# from typing import Generic, Iterator, Sequence, TypeVar, Literal, Optional
-# from langchain.schema import Document
-# from langchain_core.stores import BaseStore
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
 from langchain.retrievers import ParentDocumentRetriever
 from langchain.embeddings import CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
 from semantic_router import SemanticRouter, Route
 from semantic_router.sample import chatbotSample, chitchatSample
-# from pydantic import BaseModel, Field
 from grader import GradeDocuments
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from parent_document_retriever import PostgresStore
-# import time
-# from sqlalchemy import Column, String, create_engine
-# from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
-# from sqlalchemy.dialects.postgresql import JSONB
+
 
 load_dotenv()
 store = LocalFileStore("./cache/")
 # Define environment variables
-NGROK_URL = "0.tcp.ap.ngrok.io:11865"
+NGROK_URL = "0.tcp.ap.ngrok.io:17032"
 PG_URL = "localhost:5432"
 DB_NAME = 'parent'
 DB_USER = 'postgres'
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+# URL_STRING = f"postgresql://{DB_USER}:{DB_PASSWORD}@{NGROK_URL}/{DB_NAME}"
 URL_STRING = f"postgresql://{DB_USER}:{DB_PASSWORD}@{PG_URL}/{DB_NAME}"
 QDRANT_URL = os.getenv("QDRANT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_Key")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")    
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GPT_API_KEY = os.getenv("GPT_API_KEY")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # --- Initialize embedding model --- #
@@ -75,7 +70,7 @@ def get_vectorstore():
 
 @st.cache_resource
 def get_parent_doc_retrieve(_vector_store, _document_store, _child_splitter, _parent_splitter):
-    search_kwargs = {"k": 3}
+    search_kwargs = {"k": 5}
     parent_document_retriever = ParentDocumentRetriever(
         vectorstore=_vector_store,
         docstore=_document_store,
@@ -88,22 +83,16 @@ def get_parent_doc_retrieve(_vector_store, _document_store, _child_splitter, _pa
 # --- Initialize LLM --- #
 @st.cache_resource
 def get_llm():
-    return ChatGroq(
+    return ChatOpenAI(
+        api_key = GPT_API_KEY,
         temperature=0,
-        groq_api_key=GROQ_API_KEY,
-        # model_name="llama-3.1-8b-instant"
-        model_name="gemma2-9b-it"
+        model="gpt-4o-mini"
     )
-
-@st.cache_resource
-def get_llm_document_relevant():
-    return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
-        temperature=0,
-        max_tokens=None,
-        timeout=None,
-        max_retries=2,
-    )
+    # return ChatGroq(
+    #     temperature=0,
+    #     groq_api_key=GROQ_API_KEY,
+    #     model_name="llama-3.1-8b-instant"
+    # )
 
 # --- Create prompt template --- #
 prompt = ChatPromptTemplate.from_messages(
@@ -113,10 +102,9 @@ prompt = ChatPromptTemplate.from_messages(
             """
             Bạn là một trợ lý ảo nhiệt tình và thành thật tư vấn về luật pháp.
             Bạn sẽ dựa trên các thông tin được cung cấp để trả lời các câu hỏi của người dùng.
-            Tuyệt đối không được bịa ra câu trả lời, nếu không biết bạn phải trả lời không biết.
+            #Nếu không biết câu trả lời, bạn phải trả lời là tôi không biết.
             Dựa vào thông tin sau trả lời câu hỏi:
             {context}
-            #Nếu không có thông tin được cung cấp, bạn phải trả lời là tôi không biết.
             """
             ,
         ),
@@ -131,28 +119,7 @@ child_splitter = RecursiveCharacterTextSplitter(chunk_size=2048, chunk_overlap=5
 document_store = PostgresStore(URL_STRING)
 parent_retriever = get_parent_doc_retrieve(vector_store, document_store, child_splitter, parent_splitter)
 
-# llm = get_llm()
-llm = get_llm_document_relevant()
-llm_document_relevant = get_llm_document_relevant()
-structured_llm_grader = llm_document_relevant.with_structured_output(GradeDocuments)
-system = """
-Bạn là người đánh giá mức độ liên quan của một tài liệu đã truy xuất đến câu hỏi của người dùng. 
-Nhiệm vụ của bạn là xác định xem tài liệu có liên quan đến câu hỏi hay không.
-
-Quy tắc đánh giá:
-- Nếu tài liệu chứa thông tin liên quan đến câu hỏi: trả về {{"binary_score": "yes"}}
-- Nếu tài liệu không liên quan đến câu hỏi: trả về {{"binary_score": "no"}}
-
-Chỉ trả về một trong hai định dạng JSON trên, không thêm bất kỳ giải thích nào."""
-
-grade_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system),
-        ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
-    ]
-)
-retrieval_grader = grade_prompt | structured_llm_grader
-
+llm = get_llm()
 
 LEGAL_ROUTE_NAME = 'legal'
 CHITCHAT_ROUTE_NAME = 'normal'
@@ -160,7 +127,6 @@ CHITCHAT_ROUTE_NAME = 'normal'
 legalRoute = Route(name=LEGAL_ROUTE_NAME, samples=chatbotSample)
 chitchatRoute = Route(name=CHITCHAT_ROUTE_NAME, samples=chitchatSample)
 semanticRouter = SemanticRouter(embedding, routes=[legalRoute, chitchatRoute])
-
 
 # Streamlit UI
 st.markdown(
@@ -199,49 +165,21 @@ if user_question := st.chat_input("What is up?"):
 
             if guidedRoute == LEGAL_ROUTE_NAME:        
                 with st.spinner("Searching for relevant information..."):
-                    # retriver = vector_store.as_retriever()
-                    docs = parent_retrievergi.invoke(user_question)
+                    docs = parent_retriever.invoke(user_question)
+                    
                     # Display sources
                     with st.sidebar:
                         st.write(f"#### Num of original documents: {len(docs)}" )
                         for i, doc in enumerate(docs, 1):
                             with st.expander(f"Sources {i}: {doc.metadata['title'][:40]}..."):
-                                # st.write(f"Title: {doc.metadata['sub_title']}")
-                                # st.write(f"Date published: {doc.metadata['date_published']}")
                                 st.write(doc.page_content)
-                                # st.write(f"Keywords: {', '.join(doc.metadata['keyword'])}")
-                                # st.write(f"URL: {doc.metadata['url']}")
                                 
-                    # with st.spinner("Check relevant Documents..."):
-                    #     filtered_docs = []
-                    #     for doc in docs:
-                    #         text = doc.page_content
-                    #         try:
-                    #             result = retrieval_grader.invoke({
-                    #                 "question": user_question,
-                    #                 "document": text
-                    #             })
-                    #             if result.binary_score == 'yes':
-                    #                 filtered_docs.append(doc)
-                    #         except Exception as e:
-                    #             st.error(f"Error grading document: {str(e)}")
-                    #             continue
                     context = "\n\n".join([doc.page_content for doc in docs])
                     
                     messages = prompt.invoke({"input": user_question, "context": context})
                     with st.spinner("Generating answer..."):
                         response = llm.invoke(messages)
                     
-                    # Display sources
-                    # with st.sidebar:
-                    #     st.write(f"#### Num of filtered documents: {len(filtered_docs)}" )
-                    #     for i, doc in enumerate(filtered_docs, 1):
-                    #         with st.expander(f"Sources {i}"): #: {doc.metadata['sub_title'][:40]}..."):
-                    #             # st.write(f"Title: {doc.metadata['sub_title']}")
-                    #             # st.write(f"Date published: {doc.metadata['date_published']}")
-                    #             st.write(doc.page_content)
-                    #             # st.write(f"Keywords: {', '.join(doc.metadata['keyword'])}")
-                    #             # st.write(f"URL: {doc.metadata['url']}")
             elif guidedRoute == CHITCHAT_ROUTE_NAME:
                     messages = prompt.invoke({"input": user_question, "context": ""})
                     with st.spinner("Generating answer..."):
